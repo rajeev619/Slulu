b9a08e5b113eb65c75fef054f9306aa5e58d0494
Rajeev <rajeev.lama@selu.edu>
2018-09-23 16:40:23 -0500

    angularsetup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './login-page/login-page.component.html',
+  styleUrls: ['./login-page/login-page.component.scss']
 })
 export class AppComponent {
   title = 'SLULU';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
+    MDBBootstrapModule.forRoot(),
     BrowserModule
   ],
+  schemas: [NO_ERRORS_SCHEMA],
+
   providers: [],
   bootstrap: [AppComponent]
 })
